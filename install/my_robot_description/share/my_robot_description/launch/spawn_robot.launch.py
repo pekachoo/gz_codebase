40c945e0be8d63bb3e6a376e32e29c292a424129
pekachoo <jliu6162@gmail.com>
2024-10-26 14:49:10 -0400

    init commit

---
+++ install/my_robot_description/share/my_robot_description/launch/spawn_robot.launch.py
@@ -0,0 +1,31 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Create the absolute path to the URDF file
+    urdf_file_path = os.path.join(
+        os.getenv('HOME'), 'ros2_ws', 'src', 'my_robot_description', 'urdf', 'my_robot.urdf'
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'urdf_file',
+            default_value=urdf_file_path,
+            description='Absolute path to the URDF file'
+        ),
+
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'my_robot',
+                '-file', LaunchConfiguration('urdf_file'),
+                '-x', '0', '-y', '0', '-z', '0.5'
+            ],
+            output='screen'
+        )
+    ])
+
